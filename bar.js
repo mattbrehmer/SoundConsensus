a8c8e20d8eb3cf64c75b0e746adcb238e45d3f8e
Matt Brehmer <mattbrehmer@gmail.com>
2014-12-17 17:15:22 -0800

    facted bar chart sorted by artist

--- bar.js
+++ bar.js
@@ -1,43 +1,89 @@
-var margin = {top: 0, right: 20, bottom: 0, left: 40},
-    width = 400 - margin.left - margin.right,
-    height = 16000 - margin.top - margin.bottom;
+var margin = {top: 20, right: 20, bottom: 20, left: 20},
+    width = 3000,
+    height = 13360;
 
-var y = d3.scale.ordinal()
-    .rangeRoundBands([0, height], .2);
+var x = d3.scale.ordinal().rangePoints([0, width], 1),
+    y = {},
+    z = d3.scale.linear();
 
-var x = d3.scale.linear()
-    .range([width, 0]);
+var svg = d3.select("body")
+						.append("svg")
+    				.attr("width", width)
+    				.attr("height", height);
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
+var metadata = ["Album_url","Artist","Profile_url","Album","ReleaseDate","Label","Genre","Artist_url"]
 
-d3.csv("data-aoty/albumscores.csv", type, function(error, data) {
-  y.domain(data.map(function(d) { return d.Album; }));
-  x.domain([0, d3.max(data, function(d) { return d.AoTY; })]);
+d3.csv("data-aoty/albumscores.csv", function(error, data) { 
 
-  svg.selectAll(".bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("y", function(d) { return y(d.Album); })
-      .attr("height", y.rangeBand())
-      .attr("x", function(d) { return 0; })
-      .attr("width", function(d) { return width - x(d.AoTY); });
+	// Extract the list of dimensions and create a scale for each.
+  x.domain(dimensions = d3.keys(data[0]).filter(function(d) {
+    return metadata.indexOf(d) == -1 && (y[d] = d3.scale.linear()
+        .domain(d3.extent(data, function(p) { return +p[d]; }))
+        .range([height, 0]));
+  }));
 
-  svg.selectAll(".label")
-      .data(data)
-    .enter().append("text")
-      .attr("class", "label")
-      .attr("y", function(d) { return y(d.Album) - y.rangeBand() / 2; })
-      .attr("height", y.rangeBand() / 2)
-      .attr("x", function(d) { return 10; })
-      .attr("width", function(d) { return width - x(d.AoTY); })
-      .text(function(d) { return d.Album; } );      
+	var cell_width = width / (dimensions.length + 3) - 2;
+	var cell_height = 16;
+
+	var tmp = dimensions[1];
+	dimensions[1] = dimensions[0];
+	dimensions[0] = tmp;
+
+	z.range([0,cell_width / 2])
+	 .domain([0,100]);
+
+	var header = svg.append("g")
+								  .attr("class","header");
+
+	header.append("text")		
+			  .text("Artist / Album" )
+        .attr("transform", function(d, i) { return "translate(0," + (cell_height - 4) + ")"; });;					 
+
+  header.selectAll("columns")
+  			.data(dimensions)
+  			.enter()
+  			.append("text")
+  			.attr("transform", function(d, i) { return "translate(" + (3.5 * cell_width + i * cell_width) + "," + (cell_height - 4) + ")"; })
+  			.text(function(d) { return d; });
+
+	var table = svg.append("g")
+								 .attr("class","table")
+								 .attr("transform", function(d, i) { return "translate(0," + 20 + ")"; });
+
+	var row = table.selectAll("row")
+								 .data(data)
+								 .enter()
+								 .append("g")
+								 .attr("class", "row")
+								 .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+	row.append("text")
+     .text(function(d) { return d.Artist + " / \"" + d.Album + "\""; } )
+     .attr("transform", function(d, i) { return "translate(0," + (cell_height - 4) + ")"; });
+
+
+  var cell = row.selectAll("cell")
+					      .data(function(d) { return dimensions.map(function(k) { return d[k]; }); })
+					      .enter()
+					      .append("g")
+					      .attr("class","cell")
+					      .attr("transform", function(d, i) { return "translate(" + (3.5 * cell_width + i * cell_width) + ",0)"; })
+					      .attr("width", cell_width / 2)
+				        .attr("height", cell_height);
+
+	cell.append("rect") 	
+			.attr("class", "bounds")
+		  .attr("height", cell_height)
+		  .attr("width", cell_width / 2);					        
+
+	cell.append("rect")
+      .attr("class","value")
+      .attr("height", cell_height)
+      .attr("width", function(d) { return z(d); });
+
+  cell.append("text")
+      .attr("height", cell_height)
+      .attr("transform", function(d, i) { return "translate(4," + (cell_height - 4) + ")"; })
+      .text(function(d) { return d; });
 
 });
-
-function type(d) {
-  d.AoTY = +d.AoTY;
-  return d;
-}
